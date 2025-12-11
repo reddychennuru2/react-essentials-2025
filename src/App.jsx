00c853e0d8acbf85e4080e88914e47dc4bf89fcf
c reddy purushotham <c-reddy.purushotham@capgemini.com>
2025-12-11 16:08:32 +0530

    Moving Header component to components folder and using default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header.jsx";
 
-function Header() {
-  return (<header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>);
-}
 
 /*Props passed with props keyword */
 function CoreConcept(props) {
