9e9c0823780827155e21aebb8f1ff1f205de8a03
c reddy purushotham <c-reddy.purushotham@capgemini.com>
2025-12-16 15:30:53 +0530

    design pattern - code refactoring - using map  to dynamically add props

--- src/App.jsx
+++ src/App.jsx
@@ -61,10 +61,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul className="core-concepts-list">
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2] }/>
-            <CoreNoProp {...CORE_CONCEPTS[3]}/>
+             {CORE_CONCEPTS.map((conceptItem)=> (
+              <CoreConcept key={conceptItem.title} {...conceptItem}  />
+            ))}
           </ul>
         </section>
         <section id="examples">
