19dfee76d4f77d62cfe10efc792b9c0c94112011
c reddy purushotham <c-reddy.purushotham@capgemini.com>
2025-12-16 17:20:36 +0530

    Splitting components by feature & state - CoreConcepts & Examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { useState, Fragment } from "react";
-import { EXAMPLES } from "./data";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
 
 /*Props passed without props keyword */
 function CoreNoProp({image, title, description}) {
@@ -31,52 +29,14 @@ function Propcomponent(props) {
 function App() {
   console.log("App Executing");
 
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  let tabContent = <p>Please select a topic.</p>;
-  if (selectedTopic) {
-    tabContent = (
-      <div>
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
-  function handleSelect(SelectedTabButton) {
-    setSelectedTopic(SelectedTabButton);
-    console.log(selectedTopic);
-  }
-
-
   return (
     <>
       <Header />
       <Propcomponent title="Democomp" width={300} height="200"  />
       <main>
         <h2>Time to get started!</h2>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul className="core-concepts-list">
-             {CORE_CONCEPTS.map((conceptItem)=> (
-              <CoreConcept key={conceptItem.title} {...conceptItem}  />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            {/*using arrow functions to pass params for event handler function () => {handleSelect("xxxx")}  */}
-          <TabButton isSelected = {selectedTopic === "components"} onSelect={() => handleSelect("components")}> Components </TabButton>
-          <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}> JSX </TabButton>
-          <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleSelect("props")}> Props </TabButton>
-          <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleSelect("state")}> State </TabButton>
-          </menu>
-        </section>
-        {tabContent}
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
---
+++ src/components/Examples.jsx
@@ -0,0 +1,39 @@
+import TabButton from "../components/TabButton.jsx";
+import { useState, Fragment } from "react";
+import { EXAMPLES } from "../data.js";
+
+export default function  Examples() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+            </div>
+        );
+    }
+
+    function handleSelect(SelectedTabButton) {
+    setSelectedTopic(SelectedTabButton);
+    console.log(selectedTopic);
+    }
+
+    return (
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {/*using arrow functions to pass params for event handler function () => {handleSelect("xxxx")}  */}
+          <TabButton isSelected = {selectedTopic === "components"} onSelect={() => handleSelect("components")}> Components </TabButton>
+          <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleSelect("jsx")}> JSX </TabButton>
+          <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleSelect("props")}> Props </TabButton>
+          <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleSelect("state")}> State </TabButton>
+          </menu>
+          {tabContent}
+        </section>
+    );
+}
